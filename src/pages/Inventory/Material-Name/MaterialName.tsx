b0ee1e5e2a533bee8b76bc313c332d2707a8bff4
Abhishek624999 <abhishekarustu@gmail.com>
2025-10-10 17:44:10 +0530

    slv

--- src/pages/Inventory/Material-Name/MaterialName.tsx
+++ src/pages/Inventory/Material-Name/MaterialName.tsx
@@ -15,6 +15,7 @@ const Package = ({ className }: { className?: string }) => (
     </svg>
 );
 
+
 const Plus = ({ className }: { className?: string }) => (
     <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
